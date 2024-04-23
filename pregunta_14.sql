28f47e13c8e60dc84e17af36634672bc981efdc4
crisgomezq <161080968+crisgomezq@users.noreply.github.com>
2024-04-23 18:26:52 -0500

    INT5

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,8 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, ROUND(avg(c21), 2) AS "avg(c21)" FROM tbl1 t1, tbl2 t2 WHERE t1.K1 = t2.K1 AND c13 > 400 GROUP BY K0;
+SELECT t1.K0, ROUND(AVG(t2.c21), 2) AS "avg(c21)"
+FROM tbl1 t1
+JOIN tbl2 t2 ON t1.K1 = t2.K1
+WHERE t1.c13 > 400
+GROUP BY t1.K0;
